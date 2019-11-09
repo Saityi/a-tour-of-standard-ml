2f6109f459703af1405adbdbf9c0de9b8753ac7a
Alex Alegre <eliniel.x@gmail.com>
2019-11-09 01:19:00 -0600

    Topnav left/right buttons

--- site.hs
+++ site.hs
@@ -1,9 +1,27 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           Data.Monoid (mappend)
+import           Data.Traversable
 import           Hakyll
 import           Hakyll.Web.Redirect
 import           Data.List
 import           System.FilePath
+
+
+sortIds :: MonadMetadata m => [Identifier] -> m [Identifier]
+sortIds ids = fmap (\(_, _, i) -> i) <$> sort <$> indexed
+  where 
+    indexed = for ids $ \i -> do
+      chIdx <- getMetadataField' i "index"
+      chSec <- getMetadataField' i "section"
+      return $ (read chIdx :: Int, read chSec :: Int, i)
+
+tourPaginator :: MonadMetadata m => m Paginate
+tourPaginator = do
+  pages <- getAllMetadata "tour/*"
+  ids   <- sortIds $ fst <$> pages
+  let pageId n = ids !! (n - 1)
+      grouper ps = pure $ map (\p -> [p]) ps
+  buildPaginateWith grouper "tour/*" pageId
 
 main :: IO ()
 main = hakyll $ do 
@@ -21,22 +39,25 @@ main = hakyll $ do
     match "examples/*" $ do
         route   idRoute
         compile getResourceBody
-    
+
+    p <- tourPaginator
+
     -- Preload the tour files to make the metadata available in the sidenav
     match "tour/*" $ version "precomp" $ do
         route $ setExtension "html"
         compile $ pandocCompiler
           >>= loadAndApplyTemplate "templates/tour.html" tourContext
           >>= relativizeUrls
-    
-    match "tour/*" $ do
+  
+    paginateRules p $ \page pattern -> do
         route $ setExtension "html"
         compile $ do
           ident <- getUnderlying
           code  <- loadBody (codePath ident)
           pages <- loadAll ("tour/*" .&&. hasVersion "precomp")
           let ctx = tourContext <> listField "pages" tourContext (return pages)
-              ctx' = if (null code) then ctx else ctx <> constField "code" code
+              codectx = if (null code) then ctx else ctx <> constField "code" code
+              ctx' = codectx <> paginateContext p page
           pandocCompiler
             >>= loadAndApplyTemplate "templates/tour.html" ctx'
             >>= loadAndApplyTemplate "templates/default.html" ctx'
